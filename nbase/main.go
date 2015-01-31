1873d8cce19386b637a09b3504cd146338076b32
papplampe <papplampe@github.com>
2015-01-31 15:36:50 +0100

    nbase: big number support

--- nbase/main.go
+++ nbase/main.go
@@ -3,18 +3,19 @@ package main
 import (
 	"flag"
 	"fmt"
+	"math/big"
 	"strings"
 )
 
 var (
-	_number uint64
+	_number string
 	_string string
 	_base   uint64
 	_digits string
 )
 
 func init() {
-	flag.Uint64Var(&_number, "n", 0, "number")
+	flag.StringVar(&_number, "n", "", "number")
 	flag.StringVar(&_string, "s", "", "string")
 	flag.Uint64Var(&_base, "b", 0, "base <= length of digits string")
 	flag.StringVar(&_digits, "d",
@@ -23,55 +24,59 @@ func init() {
 	flag.Parse()
 }
 
-func tobase(n, b uint64) string {
+func tobase(n, b *big.Int) string {
+	zero := big.NewInt(0)
+	r := new(big.Int)
 	s := ""
 	for {
-		r := n % b
-		s = _digits[r:r+1] + s
-		n /= b
-		if n == 0 {
+		r.Mod(n, b)
+		s = _digits[r.Uint64():r.Uint64()+1] + s
+		n.Div(n, b)
+		if n.Cmp(zero) == 0 {
 			break
 		}
 	}
 	return s
 }
 
-func pow(n, e uint64) uint64 {
-	var i, x uint64
-	x = 1
-	for i = 0; i < e; i++ {
-		x *= n
-	}
-	return x
-}
-
-func frombase(s string, b uint64) (uint64, error) {
-	var n, e uint64
+func frombase(s string, b *big.Int) (*big.Int, error) {
+	var e uint64
+	n := new(big.Int)
+	eb := new(big.Int)
+	vb := new(big.Int)
 	for i := len(s) - 1; i >= 0; i-- {
-		v := strings.Index(_digits[:b], string(s[i]))
+		v := strings.Index(_digits[:b.Uint64()], string(s[i]))
 		if v == -1 {
-			return 0, fmt.Errorf("unknown digit %c at position %d", s[i], i)
+			return nil, fmt.Errorf("unknown digit %c at position %d", s[i], i)
 		}
-		n += uint64(v) * pow(b, uint64(e))
+		vb.SetUint64(uint64(v))
+		eb.SetUint64(e)
+		n.Add(n, vb.Mul(vb, eb.Exp(b, eb, nil)))
 		e++
 	}
 	return n, nil
 }
 
 func main() {
-	if (_number == 0 && _string == "") ||
+	if (_number == "" && _string == "") ||
 		_base == 0 || _base > uint64(len(_digits)) {
 		flag.Usage()
 		return
 	}
+	base := new(big.Int).SetUint64(_base)
 	if _string != "" {
-		n, err := frombase(_string, _base)
+		n, err := frombase(_string, base)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
 		fmt.Println(n)
 	} else {
-		fmt.Println(tobase(_number, _base))
+		n := new(big.Int)
+		if _, ok := n.SetString(_number, 10); !ok {
+			fmt.Println("number is invalid")
+			return
+		}
+		fmt.Println(tobase(n, base))
 	}
 }
