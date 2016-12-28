690ba15ecc50f35a9bf253e0ddc0a2610a4e3966
henkman <henkman@github.com>
2016-12-29 00:46:04 +0100

    https added to simplehttp

--- simplehttp/main.go
+++ simplehttp/main.go
@@ -3,18 +3,33 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"net/http"
+	"path/filepath"
+
+	"github.com/kardianos/osext"
 )
 
 var (
-	_port uint
+	_port  uint
+	_https bool
 )
 
 func init() {
 	flag.UintVar(&_port, "p", 8080, "port")
+	flag.BoolVar(&_https, "s", false, "https")
 	flag.Parse()
 }
 
 func main() {
-	http.ListenAndServe(fmt.Sprintf("0.0.0.0:%d", _port), http.FileServer(http.Dir("./")))
+	addr := fmt.Sprintf("0.0.0.0:%d", _port)
+	fs := http.FileServer(http.Dir("./"))
+	if !_https {
+		log.Fatal(http.ListenAndServe(addr, fs))
+	} else {
+		exe, _ := osext.Executable()
+		d := filepath.Dir(exe)
+		log.Fatal(http.ListenAndServeTLS(addr, filepath.Join(d, "cert.pem"), filepath.Join(d, "key.pem"), fs))
+	}
+
 }
